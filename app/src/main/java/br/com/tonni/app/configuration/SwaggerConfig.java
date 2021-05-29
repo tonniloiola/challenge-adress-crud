30c1ae4080e5ed409928889a8925ff097951cddb
Antonio <antonio.loiola@meta.com.br>
2021-05-28 22:33:11 -0300

    commit inicial

---
+++ app/src/main/java/br/com/tonni/app/configuration/SwaggerConfig.java
@@ -0,0 +1,41 @@
+package br.com.tonni.app.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    private static final String BASE_SCAN_PACKAGE = "br.com.clean.app";
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage(BASE_SCAN_PACKAGE))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(getApiInfo());
+    }
+
+    private ApiInfo getApiInfo() {
+        return new ApiInfoBuilder()
+                .title("Clean Architecture Java By Antonio")
+                .description("Spring Boot with java 11")
+                .version("1.0")
+                .contact(new Contact("Antonio", "", ""))
+                .license("Apache License Version 2.0")
+                .licenseUrl("https://www.apache.org/license/LICENSE-2.0")
+                .build();
+    }
+
+}
